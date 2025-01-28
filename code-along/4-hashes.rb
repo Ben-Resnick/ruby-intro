aa05746a9b9b1678550dd2b369b5daa963991a99
Ben Resnick <ben.resnick@kellogg.northwestern.edu>
2025-01-28 21:04:19 +0000

    finished hashes

--- code-along/4-hashes.rb
+++ code-along/4-hashes.rb
@@ -4,6 +4,26 @@
 
 # Hashes are lists of *key-value pairs*
 
+ben = {"name" => "Ben", "location" => "Chicago", "status" => "Student"}
+puts ben
+
 # Accessing data from the hash
 
+location = ben["location"]
+puts location
+
+
 # More Complex Hashes
+
+my_profile = {
+"name" => "Ben",
+"location" => {"city" => "chicago", "state" => "illinois"},
+"status" => "kellogg"
+}
+
+puts my_profile
+p my_profile["location"]["city"]
+
+
+my_profile["name"] = "Ben Resnick"
+puts my_profile
